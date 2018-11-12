67c872419837d5118bda5154265aa7a155654cdd
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-11-12 16:22:46 +0100

    * prepare some tests

--- PrimeFactors/test/PrimeFactorsTest.cpp
+++ PrimeFactors/test/PrimeFactorsTest.cpp
@@ -1,9 +1,21 @@
-#include "PrimeFactors.h"
 #include <gmock/gmock.h>
+#include "PrimeFactors.h"
 
 TEST(primeFactorsTest, testPrimeFactorsOfSomeIntegers)
 {
     using namespace ::testing;
 
-    EXPECT_THAT(PrimeFactors::getPrimeFactors(1), ElementsAre());
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(1), ElementsAre());
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(2), ElementsAre(2));
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(3), ElementsAre(3));
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(4), ElementsAre(2, 2));
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(5), ElementsAre(5));
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(6), ElementsAre(2, 3));
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(7), ElementsAre(7));
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(8), ElementsAre(2, 2, 2));
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(9), ElementsAre(3, 3));
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(10), ElementsAre(2, 5));
+    // EXPECT_THAT(PrimeFactors::getPrimeFactors(
+    //     2 * 3 * 5 * 7 * 11 * 13 * 17 * 23),
+    //     ElementsAre(2, 3, 5, 7, 11, 13, 17, 23));
 }
