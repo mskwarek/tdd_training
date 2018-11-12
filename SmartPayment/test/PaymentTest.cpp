71b148bbb28d2bff4e1f3bb7e1312befc461da37
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-11-12 16:30:49 +0100

    * added mocs to test

--- SmartPayment/test/PaymentTest.cpp
+++ SmartPayment/test/PaymentTest.cpp
@@ -1,12 +1,26 @@
 #include <gmock/gmock.h>
+#include "CardMock.hpp"
+#include "DiscountProviderMock.hpp"
+#include "Payment.h"
 
 using namespace ::testing;
 
 class PaymentTest : public Test {
+public:
+    virtual void SetUp()
+    {
+        cardMock = std::make_unique<CardMock>();
+        discountProviderMock = std::make_unique<DiscountProviderMock>();
+    }
 
-
+    void createPaymentUnderTest()
+    {
+        payment = std::make_unique<Payment>(std::move(cardMock), std::move(discountProviderMock));
+    }
+    std::unique_ptr<CardMock> cardMock;
+    std::unique_ptr<DiscountProviderMock> discountProviderMock;
+    std::unique_ptr<Payment> payment;
 };
-
 
 TEST_F(PaymentTest, can_create_payment)
 {
